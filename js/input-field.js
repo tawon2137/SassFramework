c7865f6a647d37aed755d6e129b7ab6b72216e55
tawon2137 <ziezonn@naver.com>
2017-02-12 01:42:58 +0900

    select box 제작중
    기존에 사용하던 select box의 경우 css animation이나 컴포넌트 스타일이 마음에 안들기때문에 dom을 지우고
    ul태그로 만든 select box를 고려중인데 생각보다 구성이 쉽지않다.

--- js/input-field.js
+++ js/input-field.js
@@ -1,22 +1,47 @@
 (function (win) {
-    var input = {
-        inputInit : function(){
-            var input_list = document.querySelectorAll("input:not([type]),input[type=text],input[type=password],input[type=email],input[type=url],input[type=time],input[type=date],input[type=datetime],input[type=datetime-local],input[type=tel],input[type=number],input[type=search]");
-            for (var i = 0; i < input_list.length; i++) {
-              input_list[i].addEventListener("focus",input.input_focus);
-              input_list[i].addEventListener("blur",input.input_blur);
-            }
-        },
-        input_focus : function(e){
-            tw_global.addClass(this , "active");
-        },
-        input_blur : function(e){
-            tw_global.removeClass(this,"active");
-            ( this.value.length > 0 ? tw_global.addClass(this,"valid") : ( tw_global.hasClass(this,"valid") ? tw_global.removeClass(this,"valid") : "" ) )
+      var Text_field = function () {
+          // input field 생성자
+          this.Elements = document.querySelectorAll("input:not([type]),input[type=text],input[type=password],input[type=email],input[type=url],input[type=time],input[type=date],input[type=datetime],input[type=datetime-local],input[type=tel],input[type=number],input[type=search]");
+          this.Init();
+      };
+      Text_field.prototype.Init = function(){
+          var text_fields = this.Elements;
+          for (var i = 0; i < text_fields.length; i++) {
+            text_fields[i].addEventListener("focus", this.field_focus);
+            text_fields[i].addEventListener("blur", this.field_blur);
+          }
+      };
+      Text_field.prototype.field_focus = function(){
+          tw_global.addClass(this , "active");
+      };
+      Text_field.prototype.field_blur = function(){
+        tw_global.removeClass(this,"active");
+        //중첩 3항 연산자 text_field안에 value가 존재하면 valid라는 class 를 추가하고 value가 존재하지않으면 valid 클래스를 제거해야함
+        //즉 text_field 내에 입력값이 존재하면 valid 라는 class를 추가하고 없으면 field 내에 valid 라는 클래스를 찾아서 있으면 제거하고 없으면 냅둔다.
+        (this.value.length > 0 ? tw_global.addClass(this,"valid") : ( tw_global.hasClass(this,"valid") ? tw_global.removeClass(this,"valid") : "" ));
+      };
+      var select = function(){
+          this.Elements = document.getElementsByTagName("select");
+          this.Init();
+      };
+      select.prototype.Init = function(){
+        var Elements = this.Elements;
+        for(var i = 0; i < Elements.length; i++){
+            // Elements[i].outerHTML = 
         }
-    };
+      }
+      var Construct = function(ClassObj){
+          return new ClassObj();
+      };
+      if(!win.tw_com.input){
+          win.tw_com.input = {};
+          win.tw_com.input.Construct = Construct;
+          win.tw_com.input.Text_field_class = Text_field;
+          win.tw_com.input.select_class = select;
+      }
+})(window);
 
-    if(!win.tw_com.input){
-        win.tw_com.input = input;
-    }
-})(window)
+window.addEventListener("DOMContentLoaded",function(){
+    tw_com.input.text_field = tw_com.input.Construct(tw_com.input.Text_field_class);
+    tw_com.input.select = tw_com.input.Construct(tw_com.input.select_class);
+})
