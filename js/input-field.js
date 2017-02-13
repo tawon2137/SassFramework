6cbf545aa007de9ac087f492cbddb0f782893afb
tawon2137 <ziezonn@naver.com>
2017-02-13 22:02:52 +0900

    select box 거의 완성 애니메이션 수정중

--- js/input-field.js
+++ js/input-field.js
@@ -17,47 +17,152 @@
       Text_field.prototype.field_blur = function(){
         tw_global.removeClass(this,"active");
         //중첩 3항 연산자 text_field안에 value가 존재하면 valid라는 class 를 추가하고 value가 존재하지않으면 valid 클래스를 제거해야함
-        //즉 text_field 내에 입력값이 존재하면 valid 라는 class를 추가하고 없으면 field 내에 valid 라는 클래스를 찾아서 있으면 제거하고 없으면 냅둔다.
+        //즉 text_field 내에 입력값이 존재하면 valid 라는 class를 추가하고 없으면 field 내에 valid 라는 클래스를 찾아서 있으면 제거함
         (this.value.length > 0 ? tw_global.addClass(this,"valid") : ( tw_global.hasClass(this,"valid") ? tw_global.removeClass(this,"valid") : "" ));
       };
       var select = function(){
+          this.selects = document.querySelectorAll("select:not(.browser-default)");
           this.Elements = document.getElementsByClassName("tw-select-box");
-          this.Init();
+          this.selectInit();
+          this.selectSet();
       };
-      select.prototype.Init = function(){
+      select.prototype.selectInit = function(){
+          var select_Ele_list = this.selects;
+          var self = this;
+          for (var i = 0; i < select_Ele_list.length; i++) {
+              var tw_select_box = document.createElement("div");
+              var tw_input = document.createElement("input");
+              var tw_input_line = document.createElement("hr");
+              var tw_select_ul = document.createElement("ul");
+              var down_caret = document.createElement("i");
+
+
+                for (var j = 0; j < select_Ele_list[i].options.length; j++) {
+                    var select_option =  select_Ele_list[i].options[j];
+                    var li = document.createElement("li");
+                    self.optionExtend(li , select_option , tw_input);
+                    tw_select_ul.appendChild(li);
+                }
+
+              self.attrExtends(tw_input , select_Ele_list[i]);
+              tw_select_box.className = "tw-select-box tw-input-field";
+              tw_select_ul.className = "tw-dropdown select-dropdown";
+              tw_input_line.className = "bottomline";
+              tw_input.setAttribute("readonly","true");
+              down_caret.className = "fa fa-caret-down select-caret-down";
+
+              tw_select_box.appendChild(down_caret);
+              tw_select_box.appendChild(tw_input);
+              tw_select_box.appendChild(tw_input_line);
+              tw_select_box.appendChild(tw_select_ul);
+              select_Ele_list[i].outerHTML = tw_select_box.outerHTML;
+          }
+      };
+      select.prototype.optionExtend = function(litag,  Option, input){
+          var span = document.createElement("span");
+          var value = Option.getAttribute("value") || Option.innerText;
+
+           litag.setAttribute("value", value);
+           span.innerText = Option.innerText;
+
+           if ( Option.selected ){
+             tw_global.addClass(litag , "selected");
+             input.setAttribute("value",litag.getAttribute("value"));
+           }
+
+           if ( Option.disabled ){
+             tw_global.addClass(litag ,  "disabled");
+           }
+           litag.appendChild(span);
+
+
+
+      };
+      select.prototype.attrExtends = function(childEle, parentEle){
+          childEle.id = parentEle.id;
+          childEle.className = parentEle.className + " select-input";
+          childEle.name = parentEle.name;
+      };
+      select.prototype.selectSet = function(){
         var Elements = this.Elements;
+        var self = this;
         for(var i = 0; i < Elements.length; i++){
             var select_input = Elements[i].getElementsByClassName("select-input")[0];
             var select_dropdown = Elements[i].getElementsByClassName("select-dropdown")[0];
-            console.log(select_dropdown);
-            select_input.addEventListener("focus",this.select_Open);
-            select_input.addEventListener("blur",this.select_Close);
-            select_dropdown.addEventListener("click", function(e){
-                var selectedElement = this.getElementsByClassName("selected")[0];
-                // if( )
-                if( selectedElement ){
-                  tw_global.removeClass(selectedElement, "selected");
-                }
-                tw_global.addClass(e.target, "selected");
-            });
+            select_input.addEventListener("focus",self.select_Open);
+            if ( "ontouchstart" in window ){
+              select_dropdown.addEventListener("touchstart", self.seleted);
+            }
+            select_dropdown.addEventListener("mousedown", self.seleted);
         }
       };
+
+
       select.prototype.select_Open = function(e){
          var select_element = this.parentElement;
          var select_dropdown = select_element.getElementsByClassName("select-dropdown")[0];
          var select_options = select_dropdown.getElementsByTagName("li");
-         select_dropdown.style.display = "block";
-         setTimeout(function(){
-           select_dropdown.style.opacity = "1";
+         var target = e.target || e.srcElement;
+         var listyle = select_options[0].currentStyle || window.getComputedStyle(select_options[0]);
+
+        //  var userbottom = e.target.getBoundingClientRect();
+        //  var bodybottom = document.body.getBoundingClientRect();
+        //  var interval = bodybottom.bottom - userbottom.top;
+        //  var height = parseInt(listyle.minHeight.split("px")[0]) * select_options.length ;
+         var topheight = 0;
+        //  if(interval - height < 0){
+        //     topheight = height * -1;
+        //  }
+
+         TweenLite.to( select_dropdown, 0.3, {
+             opacity : 1,
+             display : "block",
+             top : topheight,
+          });
+      };
+
+
+
+      select.prototype.seleted = function(e){
+        var targetEle = e.target || e.srcElement;
+        var self = this;
+        var dropdown_box = self.parentElement;
+        var select_input = dropdown_box.getElementsByTagName("input")[0];
+
+        if(tw_global.hasClass(targetEle,"disabled")){
+             return false;
+        }
+
+
+        var selectedElement = this.getElementsByClassName("selected")[0];
+
+        if( selectedElement ){
+          tw_global.removeClass(selectedElement, "selected");
+        }
+
+        var value = targetEle.getAttribute("value") ||  targetEle.parentElement.getAttribute("value");
+        select_input.value = value;
+        tw_global.addClass(targetEle, "selected");
+
+
+
+        TweenLite.to( self, 0.3, {
+            opacity : 0,
+            display : "none"
          });
       };
-      select.prototype.select_Close = function(e){
+
+
+
+      select.prototype.select_Close = function(){
         var select_element = this.parentElement;
         var select_dropdown = select_element.getElementsByClassName("select-dropdown")[0];
+        var css = select_dropdown.currentStyle || window.getComputedStyle(select_dropdown);
+        var delay = css["transitionDuration"].split("s,")[0] * 1000;
         select_dropdown.style.opacity = "0";
         setTimeout(function(){
            select_dropdown.style.display = "none";
-        },300);
+        },delay);
 
       };
       var Construct = function(ClassObj){
--- js/modal.js
+++ js/modal.js
@@ -13,11 +13,11 @@ var ModalConstruct = (function(){
       this.end_top = "20";
       this.end_top_suffix = "%";
       this.modalOpen = function(){
-          console.log("open");
       };
       this.modalClose = function(){
-          console.log("close");
       };
+      this.delay = 0.45;
+      this.shadow_onclick_close = false;
     };
     DefaultOption.prototype.setOption = function(newOption){
         var myOption = this;
@@ -34,20 +34,10 @@ var ModalConstruct = (function(){
        this.Css().setModal("top",(this.Option.start_top+this.Option.start_top_suffix));
        this.OpenbtnSetting(this.Element.id);
        this.ClosebtnSetting();
-       this.setEvent();
     };
 
 
-    Modal.prototype.setEvent = function(){
-        var Ele = this.Element;
-        var Ele_css = this.Css();
-        var Option = this.Option;
-        Ele.addEventListener("transitionend",function (e) {
-            if(e.propertyName === "opacity"){
-              Ele_css.getModal("opacity") === "1" ? Option.modalOpen() : Option.modalClose()
-            }
-        });
-    };
+
 
     Modal.prototype.OpenbtnSetting = function(){
       var id = this.Element.id;
@@ -81,7 +71,7 @@ var ModalConstruct = (function(){
           modal.createShadow(target);
           modal.OpenAnimation();
         }else{
-          throw new Error("Error asd");
+          throw new Error("Error Modal Open Error");
         }
     };
     Modal.prototype.closeModal = function(e){
@@ -92,40 +82,44 @@ var ModalConstruct = (function(){
       if(modal){
           modal.closeAnimation();
       }else{
-        throw new Error("Error asd");
+        throw new Error("Error Modal Close Error");
       }
     };
 
     Modal.prototype.OpenAnimation = function(){
          var Ele_css = this.Css();
-         var Shadow_css = this.shadowEle.style;
+         var Shadow_css = this.shadowEle;
          var css_option = this.Option;
-          Ele_css.setModal("display" , "block");
-          requestAnimationFrame(function(){
-            Ele_css.setModal("top" , (css_option.end_top+css_option.end_top_suffix));
-            Ele_css.setModal("opacity" , "1");
-            Ele_css.setModal("transform" , "scaleX(1)");
-            Shadow_css.opacity = css_option.shadow_opacity;
+         var Ele = this.Element;
+         TweenLite.to( Shadow_css, css_option.delay, { opacity : css_option.shadow_opacity } );
+         TweenLite.to( Ele, css_option.delay, {
+           display:"block",
+           top: (css_option.end_top+css_option.end_top_suffix),
+           scaleX : 1 ,
+           opacity : 1,
+           ease: Power3.easeOut,
+           onComplete:this.Option.modalOpen
           });
     };
 
     Modal.prototype.closeAnimation = function(){
-         var Ele_css = this.Css();
-         var Shadow_css = this.shadowEle.style;
+         var Shadow_css = this.shadowEle;
          var css_option = this.Option;
-         var delay = this.Css().getModal("transitionDuration").split("s,")[0] * 500;
-
-          requestAnimationFrame(function(){
-            Ele_css.setModal("top" , (css_option.start_top+css_option.start_top_suffix));
-            Ele_css.setModal("opacity" , "0");
-            Ele_css.setModal("transform" , "scaleX(.7)");
-            Shadow_css.opacity = "0";
-            setTimeout(function(){
-                Ele_css.setModal("display", "none");
-            },delay);
-          });
+         var Ele = this.Element;
+        TweenLite.to( Shadow_css, css_option.delay, { opacity : 0 } );
+        TweenLite.to(Ele, css_option.delay,
+          {
+          display:"none",
+          top: (css_option.start_top+css_option.start_top_suffix),
+          scaleX : 0.7,
+          opacity : 0,
+          ease: Power3.easeOut,
+          onComplete:function(){
+          Shadow_css.parentElement.removeChild(Shadow_css);
+          css_option.modalClose();
+          }
+      });
     };
-
     Modal.prototype.Css = function(){
         var Ele = this.Element;
         return {
@@ -147,15 +141,11 @@ var ModalConstruct = (function(){
     };
     Modal.prototype.createShadow = function(id){
         var Shadowele = document.createElement("div");
+        var Option = this.Option;
         Shadowele.id = "modal-shadow";
         Shadowele.setAttribute("modal-target",id);
-        Shadowele.addEventListener("click",this.closeModal);
-        Shadowele.addEventListener("transitionend",function(e){
-           var css = this.currentStyle || window.getComputedStyle(this);
-             if(css.opacity === "0"){
-                this.parentElement.removeChild(this);
-             }
-        });
+        //옵션에서 준 Shadow_onclick_close  값에 따라 true면 그림자영역 클릭시 모달 close / false 이면 그림자영역 클릭이벤트를 설정하지않음.
+        ( Option.shadow_onclick_close ? Shadowele.addEventListener("click",this.closeModal) : "" );
         this.shadowEle = Shadowele;
         return document.body.appendChild(Shadowele);
     };
